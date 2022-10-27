49b45b65df2e08390529c12c83c9b369ee73f7a5
jibananando <jibananando.pramanik@gmail.com>
2022-10-27 14:51:59 +0600

    server creat api then add server and showed dada ui by useEfffect

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+            .then(res => res.json())
+            .then(data => setCategories(data));
+    }, [])
+
+
     return (
         <div>
-            <h3>Left side nav</h3>
+            <h4>All Category: {categories.length}</h4>
+            <div>
+                {
+                    categories.map(category => <p key={category.id}>
+                        <Link>{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
